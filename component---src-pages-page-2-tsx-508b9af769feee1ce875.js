466365d4f6fa359eb6450cfc1463e1dfe100d1c5
Automatic build and deploy <circleci@716165c46278>
2020-01-02 22:10:35 +0000

    Updates

---
+++ component---src-pages-page-2-tsx-508b9af769feee1ce875.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[10],{208:function(e,t,a){"use strict";a.r(t);var n=a(0),o=a.n(n),l=a(226),c=a(255),r=a(248);t.default=function(){return o.a.createElement(c.e,null,o.a.createElement(r.a,{title:"Page two"}),o.a.createElement("h1",null,"Hi from the second page"),o.a.createElement("p",null,"Welcome to page 2"),o.a.createElement(l.a,{to:"/"},"Go back to the homepage"))}}}]);
+//# sourceMappingURL=component---src-pages-page-2-tsx-508b9af769feee1ce875.js.map
