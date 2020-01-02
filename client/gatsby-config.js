db45892537f789ddf1ffdde62990d42a09555cdd
scientistcoco <courtney.lum@gmail.com>
2020-01-02 20:41:05 +1100

    Change site icon

--- client/gatsby-config.js
+++ client/gatsby-config.js
@@ -1,9 +1,8 @@
 module.exports = {
   pathPrefix: "/Aelan-Place",  
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Aelan Place`,
+    description: `Tropical apartments`,    
   },
   plugins: [
     `gatsby-plugin-typescript`,
@@ -29,7 +28,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/aelan-place.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
