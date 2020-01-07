709089c83c46e94fdad1079d55b4602c15c6daf4
scientistcoco <courtney.lum@gmail.com>
2020-01-07 21:35:48 +1100

    Remove line

--- server/routes/healthcheck.ts
+++ server/routes/healthcheck.ts
@@ -2,7 +2,6 @@ import { Router, Response, Request } from 'express';
 
 const healthCheckRouter = Router();
 
-
 healthCheckRouter.get('/', (req: Request, res: Response) => {
   return res.json({ message: 'Success '});
 });
