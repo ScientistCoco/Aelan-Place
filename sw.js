1b48ff1c5d7063ba93dc54dd8262118299e8870f
Automatic build and deploy <circleci@716165c46278>
2021-04-21 11:21:51 +0000

    Updates

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-17b5946bb4159bc7aa30.js"
+    "url": "webpack-runtime-3989c55388b027b21459.js"
   },
   {
     "url": "netlify-identity-widget-adf2877c9f40e7f7f0ef.js"
@@ -39,11 +39,11 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "a89204891d7f8163612d0866586c3ce0"
+    "revision": "17a38d6624dd26e21892952302cfda30"
   },
   {
     "url": "page-data/offline-plugin-app-shell-fallback/page-data.json",
-    "revision": "353619eb5e7dd63f6affcc554f536fe2"
+    "revision": "19a766a12e51844e486aa17fb76c275a"
   },
   {
     "url": "manifest.webmanifest",
